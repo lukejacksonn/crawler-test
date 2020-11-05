8f960899fde618be3d4dcda5a54e3ac8caa14174
Jovi De Croock <decroockjovi@gmail.com>
2020-11-05 19:38:40 +0100

    testing crawlers

---
+++ src/index.js
@@ -0,0 +1,37 @@
+  
+import { createElement as h, render } from 'https://unpkg.com/preact@latest?module';
+import htm from 'https://unpkg.com/htm@latest?module';
+import { useTitle, } from 'https://unpkg.com/hooked-head@latest?module';
+
+window.html = htm.bind(h);
+
+const App = () => {
+  useEffect(() => {
+    document.title = 'Is this working?';
+    const twitterTag = document.createElement('meta');
+    twitterTag.setAttribute('twitter:title', 'crawler-test');
+    document.head.appendChild(twitterTag)
+
+    const facebookTag = document.createElement('meta');
+    facebookTag.setAttribute('og:title', 'crawler-test');
+    document.head.appendChild(facebookTag)
+
+    const descriptionTag = document.createElement('meta');
+    descriptionTag.setAttribute('description', 'testing crawlers');
+    document.head.appendChild(descriptionTag)
+  }, [])
+  return html`
+    <div>
+      <h1>This is a nice website yes!</h1>
+      <p>
+        We are testing whether or not a Google/FB-crawler will correctly spot this website.!
+      </p>
+    </div>
+  `
+}
+
+
+render(
+  html`<${App} />`,
+  document.body,
+);
