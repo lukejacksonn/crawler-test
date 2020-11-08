a411fa967e95be627290eadb853bb16d2cc34bad
lukejacksonn <lukejacksonn@gmail.com>
2020-11-08 14:35:47 +0000

    Use documentElement as the root node

--- src/index.js
+++ src/index.js
@@ -1,37 +1,60 @@
-  
-import { createElement as h, render } from 'https://unpkg.com/preact@latest?module';
-import { useEffect } from 'https://unpkg.com/preact@10.5.5/hooks/dist/hooks.module.js?module';
+import {
+  createElement as h,
+  render,
+} from 'https://unpkg.com/preact@latest?module';
+import {
+  useEffect,
+  useState,
+} from 'https://unpkg.com/preact@10.5.5/hooks/dist/hooks.module.js?module';
 import htm from 'https://unpkg.com/htm@latest?module';
 
 window.html = htm.bind(h);
 
 const App = () => {
+  // Fetching dynamic data
+  const [meta, setMeta] = useState({});
   useEffect(() => {
-    document.title = 'Is this working?';
-    const twitterTag = document.createElement('meta');
-    twitterTag.setAttribute('twitter:title', 'crawler-test');
-    document.head.appendChild(twitterTag)
+    fetch('https://api.github.com/repos/JoviDeCroock/crawler-test')
+      .then((res) => res.json())
+      .then(setMeta);
+  }, []);
 
-    const facebookTag = document.createElement('meta');
-    facebookTag.setAttribute('og:title', 'crawler-test');
-    document.head.appendChild(facebookTag)
-
-    const descriptionTag = document.createElement('meta');
-    descriptionTag.setAttribute('description', 'testing crawlers');
-    document.head.appendChild(descriptionTag)
-  }, [])
+  // Rendering some static then dynamic data
   return html`
-    <div>
-      <h1>This is a nice website yes!</h1>
-      <p>
-        We are testing whether or not a Google/FB-crawler will correctly spot this website.!
-      </p>
-    </div>
-  `
-}
+    <head>
+      <meta charset="utf-8" />
+      <title>Testing Crawlers</title>
+      <meta
+        name="viewport"
+        content="width=device-width, initial-scale=1, shrink-to-fit=no"
+      />
+      <meta name="description" content="calling all crawlers" />
+      <meta twitter:title="crawler-test" />
+      <meta og:title="crawler-test" />
+    </head>
+    <body>
+      <header>
+        <h1>This is a nice website yes!</h1>
+        <p>
+          We are testing whether or not a Google/FB-crawler will correctly spot
+          this website!
+        </p>
+      </header>
+      <main>
+        <h1>Repo: ${meta.name}</h1>
+        <img
+          width="180"
+          height="180"
+          alt="Photo of ${meta && meta.owner && meta.owner.login}"
+          src=${meta && meta.owner && meta.owner.avatar_url}
+        />
+        <p>ID: <b>${meta.id}</b></p>
+        <p>Forks: <b>${meta.forks}</b></p>
+        <p>Stars: <b>${meta.stargazers_count}</b></p>
+      </main>
+      <footer><h1>Thanks for coming</h1></footer>
+    </body>
+  `;
+};
 
-
-render(
-  html`<${App} />`,
-  document.body,
-);
+render(html`<${App} />`, document.documentElement);
